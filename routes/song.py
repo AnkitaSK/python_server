9f69e74451cb6f63e776551b5c131a1e50d999ef
ankitask <ankitakalangutkar90@gmail.com>
2024-08-09 15:17:47 +0200

    Upload and save song and thumbnail

--- routes/song.py
+++ routes/song.py
@@ -8,6 +8,8 @@ import cloudinary
 import cloudinary.uploader
 from cloudinary.utils import cloudinary_url
 
+from models.song import Song
+
 router = APIRouter()
 
 cloudinary.config( 
@@ -17,7 +19,7 @@ cloudinary.config(
     secure=True
 )
 
-@router.post('/upload')
+@router.post('/upload',status_code=201)
 def upload_song(song: UploadFile = File(...),
                 thumbnail: UploadFile = File(...),
                 artist: str = Form(...),
@@ -28,8 +30,18 @@ def upload_song(song: UploadFile = File(...),
                 ):
     song_id = str(uuid.uuid4())
     song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
-    print(song_res)
     thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
-    print(thumbnail_res)
-    # store data into db
-    return 'ok'
+    
+    new_song = Song(
+        id=song_id,
+        song_name=song_name,
+        artist=artist,
+        hex_code= hex_code,
+        song_url=song_res['url'],
+        thumbnail_url=thumbnail_res['url'],
+    )
+
+    db.add(new_song)
+    db.commit()
+    db.refresh(new_song)
+    return new_song
